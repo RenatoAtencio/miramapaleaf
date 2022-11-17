0342d066173d7de65a8e0c2dc37f7b383ed4edd2
Renato Atencio <renatoatencioaedo@gmail.com>
2022-11-16 22:26:39 -0300

    cambio

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,8 +1,14 @@
 import React from "react"
-import {MapContainer,TileLayer} from "react-leaflet"
+import {MapContainer,Marker,TileLayer} from "react-leaflet"
 import 'leaflet/dist/leaflet.css'
-import Markers from "./Markers"
-import edifInfo from "../data/data.json"
+import L from 'leaflet'
+import edif from "../data/data.json"
+
+const markerIcon = new L.icon({
+  iconUrl:require("../assets/Marker.png"),
+  iconSize:[35,35],
+  iconAnchor:null,
+})
 
 const MapView = () => {
   return <MapContainer center={[-39.831742, -73.246750]} zoom={16}> 
@@ -10,7 +16,18 @@ const MapView = () => {
       url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     />
-    <Markers edif={edifInfo.edif}/>
+    
+    {edif.map((edif,idx)=>
+      <Marker 
+        position={[edif.marcador.lat,edif.marcador.lng]} 
+        icon={markerIcon}
+        key={idx}
+      >
+
+      </Marker>
+    )}
+
+
   </MapContainer>
 }
 
