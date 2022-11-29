5d6737867b0da1a1ff808282406351d794a4b333
Renato Atencio <renatoatencioaedo@gmail.com>
2022-11-29 10:43:22 -0300

    cambio en el diseño targeta

--- src/components/Informacion.js
+++ src/components/Informacion.js
@@ -1,15 +1,16 @@
-import React,{useState} from 'react';
+import React from 'react';
+import '../assets/Targeta.css'
 
 export default function Informacion(props){
   const { selectPosition } = props;
   var nom = selectPosition?.ident;
   console.log(nom);
   return (
-    <div>
+    <div className='Targeta'>
       <div className='TargetaTitulo'>
         <h4>{selectPosition?.ident}</h4>
       </div>
-      <div>
+      <div className='LinkPagina'>
         <a>link a la pagina con informacion</a>
       </div>
       <div className='TargetaInformacion'>
