43b06ed43e0a9fd40072cdee2471d17bfcf53868
Renato Atencio <renatoatencioaedo@gmail.com>
2022-12-15 22:29:53 -0300

    Cambio en las paginas

--- src/components/Edificio.js
+++ src/components/Edificio.js
@@ -1,60 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
-import { OutlinedInput } from "@mui/material";
-import Button from "@mui/material/Button";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import Divider from "@mui/material/Divider";
+import "../assets/estilos/Informacion.css";
 
+const IMAGENES = require.context("../assets/fotos", true);
 export default function Edificio() {
   const location = useLocation();
   const { selectPosition } = location.state;
-
-  console.log(selectPosition);
   return (
-    <div>
-      <div>
-        <p>{selectPosition.ident}</p>
+    <div className="BaseInformacion">
+      <div className="SeccionDeSeleccion">
+        <div className="NombreEdificio">
+          <p>{selectPosition.ident}</p>
+        </div>
+        <div className="Lista">
+          <List>
+            {selectPosition.pisos.map((item) => {
+              return (
+                <div key={item?.numero} className="NombreSalas">
+                  <ListItem button>
+                    <ListItemText primary={item?.numero} />
+                  </ListItem>
+                </div>
+              );
+            })}
+          </List>
+        </div>
       </div>
-      <div>
-        <p>{selectPosition.informacion.texto}</p>
-      </div>
-      <div >
-        <List>
-          {selectPosition.pisos.map((item)=> {
-            return(
-              <div key={item?.numero}>
-                <ListItem button>
-                  <ListItemText primary={item?.numero} />
-                </ListItem>
-                <Divider />
-              </div>
-            )
-          })}
-        </List>
+      <div className="SeccionDeInformacion">
+        <div className="SeccionDeFotos">
+          <img
+            src={IMAGENES(`./${selectPosition?.informacion.foto}.jpg`)}
+            alt={selectPosition?.informacion.foto}
+          />
+        </div>
+        <div className="TextoInformacion">
+          <div className="InformacionSeleccionada">
+            <p>{selectPosition.informacion.texto}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
 }
-
-
-/**
- * <List>
-          {Edificios.map((item) => {
-            return (
-              <div key={item?.ident}>
-                <ListItem
-                  button
-                  onClick={() => {
-                    setSelectPosition(item);
-                  }}
-                >
-                  <ListItemText primary={item?.ident} />
-                </ListItem>
-                <Divider />
-              </div>
-            );
-          })}
-        </List>
- */
--- src/components/Informacion.js
+++ src/components/Informacion.js
@@ -2,12 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../assets/estilos/Targeta.css";
 
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import Divider from "@mui/material/Divider";
+
 export default function Informacion(props) {
   const { selectPosition } = props;
 
   /**Esta parte es para la targeta de informacion que cambia segun
    * el edificio seleccionado, hay un if para que cuando se comienze
-   * la app muestre una targeta de base, luego de eso se actualiza 
+   * la app muestre una targeta de base, luego de eso se actualiza
    * cada vez que se selecciona un edificio nuevo
    */
   if (selectPosition?.ident === undefined) {
@@ -24,14 +29,27 @@ export default function Informacion(props) {
         </div>
         <div className="LinkPagina">
           <Link to="/Edificio" state={{ selectPosition }}>
-            Ir!
+            Informacion del Edificio
           </Link>
         </div>
-        <div className="TargetaInformacion">
-          <p>{selectPosition?.informacion.texto}</p>
-        </div>
-        <div className="Fotos">
-          <p>{selectPosition?.informacion.foto}</p>
+        <div className="InformacionSalas">
+          <div className="TituloInformacionSalas">
+            <h4>Salas en el edificio</h4>
+          </div>
+          <div className="ListaDeLasSalas">
+            <List>
+              {selectPosition.pisos.map((item) => {
+                return (
+                  <div key={item?.numero}>
+                    <ListItem>
+                      <ListItemText primary={item?.numero} />
+                    </ListItem>
+                    <Divider />
+                  </div>
+                );
+              })}
+            </List>
+          </div>
         </div>
       </div>
     );
