f5731f37e8baa7bbfa53002c54eaf8deeec3b4fe
Renato Atencio <renatoatencioaedo@gmail.com>
2022-12-17 15:40:28 -0300

    autocompletacion

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -12,12 +12,9 @@ export default function SearchBar(props) {
   const { selectPosition, setSelectPosition } = props;
   const [searchText, setSearchText] = useState("");
   return (
-    /**Seccion de la busqueda de salas */
     <div className="MenuDeBusqueda">
-      {/**Esta parte es el input con el boton, para ambos
-        se estan usando componentes de mui (parecido a chakra) */}
       <div className="BarraBoton">
-        <div>
+        <div className="InputBusqueda">
           <OutlinedInput
             style={{ width: "100%" }}
             value={searchText}
@@ -26,16 +23,94 @@ export default function SearchBar(props) {
             }}
           />
         </div>
-        <div>
+        <div className="BotonBusqueda">
           <Button variant="contained">Buscar</Button>
         </div>
       </div>
-      {/**Esta parte es para la lista de los edificios,
-          hay una funcion que recorre el json y crea items (que son los edifios)
-          y les asigna el nombre (ident) del edificio */}
-      <div>
+      <div className="dropdown">
         <List>
-          {Edificios.map((item) => {
+          {Edificios.filter((item) => {
+            const lugarBuscado = searchText.toLowerCase();
+            const edificioBuscado = item.ident.toLowerCase();
+            {
+              /*const salaBuscada = item.ident.pisos.numero.toLowerCase();*/
+            }
+            return (
+              lugarBuscado &&
+              edificioBuscado.startsWith(lugarBuscado) &&
+              edificioBuscado !== lugarBuscado
+            );
+          })
+            .slice(0, 10)
+            .map((item) => (
+              <div
+                onClick={(event) => {
+                  setSearchText(event.target.value);
+                }}
+                className="dropdown-row"
+                key={item.ident}
+              >
+                {item.ident}
+              </div>
+            ))}
+        </List>
+      </div>
+    </div>
+  );
+}
+/**
+ * export default function App() {
+  const [value, setValue] = useState("");
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const onSearch = (searchTerm) => {
+    setValue(searchTerm);
+    // our api to fetch the search result
+    console.log("search ", searchTerm);
+  };
+
+  return (
+    <div className="App">
+      <h1>Search</h1>
+
+      <div className="search-container">
+        <div className="search-inner">
+          <input type="text" value={value} onChange={onChange} />
+          <button onClick={() => onSearch(value)}> Search </button>
+        </div>
+        <div className="dropdown">
+          {data
+            .filter((item) => {
+              const searchTerm = value.toLowerCase();
+              const fullName = item.ident.toLowerCase();
+              const sas = item.wea.toLowerCase();
+
+              return (
+                searchTerm &&
+                (sas.startsWith(searchTerm) ||
+                  fullName.startsWith(searchTerm)) &&
+                fullName !== searchTerm
+              );
+            })
+            .slice(0, 10)
+            .map((item) => (
+              <div
+                onClick={() => onSearch(item.ident)}
+                className="dropdown-row"
+                key={item.ident}
+              >
+                {item.ident}
+              </div>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+{Edificios.map((item) => {
             return (
               <div key={item?.ident}>
                 <ListItem
@@ -50,8 +125,7 @@ export default function SearchBar(props) {
               </div>
             );
           })}
-        </List>
-      </div>
-    </div>
-  );
-}
+
+
+
+ */
